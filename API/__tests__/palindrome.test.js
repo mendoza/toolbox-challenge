9e3ba81091920c09605426e0732aef45d86a142e
David Mendoza <d.mendoza99@unitec.edu>
2021-03-10 15:31:02 -0600

    simplifique el script de test y tambien los test para usar mas chai

--- API/__tests__/palindrome.test.js
+++ API/__tests__/palindrome.test.js
@@ -3,38 +3,32 @@ const { expect } = require('chai')
 
 describe('GET /iecho?text=test', () => {
   it('Should return status 200 and the text should be turn around and the palindrome flag being false', () => {
-    request
-      .get('/iecho?text=test')
-      .expect(200)
-      .then((response) => {
-        expect(response.body.text).to.eql('tset')
-        expect(response.body.palindrome).to.eql(false)
-        done()
-      })
+    request.get('/iecho?text=test').then((response) => {
+      expect(response.status).to.eql(200)
+      expect(response.body.text).to.eql('tset')
+      expect(response.body.palindrome).to.eql(false)
+      done()
+    })
   })
 })
 
 describe('GET /iecho?text=ana', () => {
   it('Should return status 200 and the text should be turn around and the palindrome flag being true', () => {
-    request
-      .get('/iecho?text=test')
-      .expect(200)
-      .then((response) => {
-        expect(response.body.text).to.eql('ana')
-        expect(response.body.palindrome).to.eql(true)
-        done()
-      })
+    request.get('/iecho?text=test').then((response) => {
+      expect(response.status).to.eql(200)
+      expect(response.body.text).to.eql('ana')
+      expect(response.body.palindrome).to.eql(true)
+      done()
+    })
   })
 })
 
 describe('GET /iecho', () => {
   it("Should return status 400 because it doesn't have text", () => {
-    request
-      .get('/iecho')
-      .expect(400)
-      .then((response) => {
-        expect(response.body.error).to.eql('no text')
-        done()
-      })
+    request.get('/iecho').then((response) => {
+      expect(response.status).to.eql(400)
+      expect(response.body.error).to.eql('no text')
+      done()
+    })
   })
 })
