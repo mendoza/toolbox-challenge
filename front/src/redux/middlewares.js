d77c46287ee8496fdbdbf8cd3192420234e2836c
David Mendoza <d.mendoza99@unitec.edu>
2021-03-10 19:17:54 -0600

    hice un middleware para redux para poder hacer mis llamadas al api

---
+++ front/src/redux/middlewares.js
@@ -0,0 +1,19 @@
+import { ADD_WORD, TEST_WORD } from "./actionTypes";
+import axios from "axios";
+
+const asyncTestWord = (storeAPI) => (next) => (action) => {
+  if (action.type === TEST_WORD) {
+    const { word } = action.payload;
+    axios
+      .get(`http://localhost:3001/iecho?text=${word}`)
+      .then((resp) => {
+        storeAPI.dispatch({ type: ADD_WORD, payload: { word: resp.data } });
+      })
+      .catch(() => {
+        console.log("wenas");
+      });
+  }
+  return next(action);
+};
+
+export { asyncTestWord };
